13cfce50ca4dffe846b50f32a3bac06e6ca813fb
nagesh-rathod-9 <nagesh13212@gmail.com>
2024-05-04 15:55:24 +0530

    first commit

--- route/admin.js
+++ route/admin.js
@@ -2,10 +2,20 @@ var express=require("express")
 var route=express.Router()
 var exe=require("./connection")
 
-route.get("/",function(req,res){
-    res.render("admin/home.ejs")
+function check_admin(req,res,next){
+  if(req.session.admin_id==undefined){
+    res.redirect("/admin/login")
+  }
+  if(req.session.admin_id!= undefined){
+    next()
+}
+}
+
+route.get("/",check_admin,async function(req,res){
+    var contact_form=await exe(`SELECT * FROM contact_form`)
+    res.render("admin/home.ejs",{"contact_form":contact_form})
 })
-route.get("/home_detail",async function(req,res){
+route.get("/home_detail",check_admin,async function(req,res){
     var data=await exe(`SELECT * FROM home`);
     var social=await exe(`SELECT * FROM social`);
 
@@ -31,9 +41,13 @@ route.post("/social_link",async function(req,res){
     await exe(sql)
     res.redirect("/admin/home_detail")
 })
-route.get("/about", async function(req,res){
+route.get("/about",check_admin, async function(req,res){
     var about_detail=await exe(`SELECT * FROM about_detail`)
-    res.render("admin/about.ejs",{"about_detail":about_detail})
+    var service_list=await exe(`SELECT * FROM service`)
+    var interest=await exe(`SELECT * FROM interest`)
+    var programming=await exe(`SELECT * FROM programming`)
+
+    res.render("admin/about.ejs",{"about_detail":about_detail,"service_list":service_list,"interest":interest,"programming":programming})
 })
 route.post("/save_about",async function(req,res){
     var d=req.body;
@@ -48,11 +62,11 @@ route.post("/save_service",async function(req,res){
     res.redirect("/admin/about")
     
 })
-route.get("/service_list",async function(req,res){
+route.get("/service_list",check_admin,async function(req,res){
     var service_list=await exe(`SELECT * FROM service`)
     res.render("admin/service_list.ejs",{"service_list":service_list})
 })
-route.get("/edit/:service_id",async function(req,res){
+route.get("/edit/:service_id",check_admin,async function(req,res){
     var service_list=await exe(`SELECT * FROM service WHERE service_id='${req.params.service_id}'`)
     res.render("admin/edit_service.ejs",{"service_list":service_list})
 })
@@ -60,12 +74,12 @@ route.post("/edit/update_ser",async function(req,res){
     var d=req.body;
     var update_ser=`UPDATE service SET service='${d.service}' WHERE service_id='${d.hidd}'`
     await exe(update_ser)
-    res.redirect("/admin/service_list")
+    res.redirect("/admin/about")
 })
 route.get("/delete/:id",async function(req,res){
     var sql=`DELETE FROM service WHERE service_id='${req.params.id}'`
     await exe(sql)
-    res.redirect("/admin/service_list")
+    res.redirect("/admin/about")
 })
 route.post("/intrest",async function(req,res){
     var d=req.body
@@ -73,8 +87,98 @@ route.post("/intrest",async function(req,res){
     await exe(sql);
     res.redirect("/admin/about")
 })
-route.get("/interst_list",async function(req,res){
-    res.render("admin/interest.ejs")
+
+route.get("/edit_interest/:id",async function(req,res){
+    var interest=await exe(`SELECT * FROM interest WHERE interest_id='${req.params.id}'`)
+    res.render("admin/edit_interest.ejs",{"interest":interest})
+})
+
+route.post("/edit_int",async function(req,res){
+    var d=req.body;
+    await exe(`UPDATE interest SET interest='${d.intrest}' WHERE interest_id='${d.intrest_id}'`)
+    res.redirect("/admin/about")
+})
+route.get("/delete_intrest/:id",check_admin,async function(req,res){
+    await exe(`DELETE FROM interest WHERE interest_id='${req.params.id}'`)
+    res.redirect("/admin/about")
+})
+route.post("/save_programming",async function(req,res){
+    var d=req.body;
+    await exe(`INSERT INTO programming(t_name,percentage) VALUES('${d.t_name}','${d.percentage}')`)
+    res.redirect("/admin/about")
+})
+route.get("/edit_tech/:id",check_admin,async function(req,res){
+    var programming=await exe(`SELECT * FROM programming WHERE p_id='${req.params.id}'`)
+    res.render("admin/edit_technology.ejs",{"programming":programming})
+})
+route.post("/edit_programming",async function(req,res){
+    var d=req.body;
+    await exe(`UPDATE programming SET t_name='${d.t_name}',percentage='${d.percentage}' WHERE p_id='${d.p_id}'`)
+    res.redirect("/admin/about")
+})
+route.get("/delete_tech/:id",check_admin,async function(req,res){
+    await exe(`DELETE FROM programming WHERE p_id='${req.params.id}'`)
+    res.redirect("/admin/about")
+})
+route.get("/project",check_admin,async function(req,res){
+    var project=await exe(`SELECT * FROM project`)
+    res.render("admin/projects.ejs",{"project":project})
+})
+route.post("/save_project",async function(req,res){
+    var d=req.body;
+    var img=new Date().getTime()+req.files.img.name;
+    req.files.img.mv("public/uploads/"+img)
+    await exe(`INSERT INTO project(img,project_link,project_heading,project_disc,project_type,project_date) VALUES ('${img}','${d.link}','${d.heading}','${d.disc}','${d.type}','${d.date}')`)
+    res.redirect('/admin/project')
+})
+route.get("/edit_project/:id",check_admin,async function(req,res){
+    var project=await exe(`SELECT * FROM project WHERE project_id='${req.params.id}'`)
+    res.render("admin/edit_project.ejs",{"project":project})
+})
+route.post("/edit_project",async function(req,res){
+    var d=req.body;
+    if(req.files!=undefined){
+        if(req.files.img){
+            var img=new Date().getTime()+req.files.img.name;
+            req.files.img.mv("public/uploads/"+img)
+            await exe(`UPDATE project SET img ='${img}' WHERE project_id='${d.project_id}'`)
+        }
+    }
+    await exe(`UPDATE project SET project_link='${d.link}',project_heading='${d.heading}',project_disc='${d.disc}',project_type='${d.type}',project_date='${d.date}' WHERE project_id='${d.project_id}'`)
+    res.redirect("/admin/project")
+})
+route.get("/delete_project/:id",check_admin,async function(req,res){
+    await exe(`DELETE FROM project WHERE project_id='${req.params.id}'`)
+    res.redirect("/admin/project")
+
+})
+route.get("/contact_info",check_admin,async function(req,res){
+    var basic_info=await exe(`SELECT * FROM basic_info`)
+    res.render("admin/contact_us.ejs",{"basic_info":basic_info})
+})
+route.post("/save_basic_info",async function(req,res){
+    var d=req.body;
+    // await exe(`INSERT INTO basic_info(address,email,mobile) VALUES ('${d.address}','${d.email}','${d.mobile}')`)
+    await exe(`UPDATE basic_info SET address='${d.address}',email='${d.email}',mobile='${d.mobile}',map='${d.map}'`)
+    res.redirect("/admin/contact_info")
+})
+route.get("/delete_contact/:id",check_admin,async function(req,res){
+   await exe(`DELETE FROM contact_form WHERE c_id='${req.params.id}'`)
+    res.redirect("/admin")
+})
+route.get("/login",function(req,res){
+    res.render("admin/login.ejs")
+})
+route.post("/check_admin",async function(req,res){
+    var d=req.body
+    var info=await exe(`SELECT * FROM admin WHERE username='${d.username}' AND password='${d.password}'`)
+    if(info.length>0){
+        req.session['admin_id']=info[0].admin_id;
+        res.redirect("/admin")
+    }
+    else{
+        res.redirect('/admin/login')
+    }
 })
 module.exports=route;
 
@@ -83,3 +187,6 @@ module.exports=route;
 // CREATE TABLE about_detail(about_id INT PRIMARY KEY AUTO_INCREMENT,disc_1 TEXT,disc_2 TEXT,name TEXT,address TEXT,study TEXT,degree TEXT,mail TEXT,phone VARCHAR(13))
 //CREATE TBALE service(service_id INT PRIMARY KEY AUTO_INCREMENT,service VARCHAR(200))
 //CREATE TBALE interest(interest_id INT PRIMARY KEY AUTO_INCREMENT,interest VARCHAR(200))
+// CREATE TABLE programming(p_id INT PRIMARY KEY AUTO_INCREMENT,t_name TEXT, percentage TEXT)
+// CREATE TABLE project(project_id INT PRIMARY KEY AUTO_INCREMENT,img TEXT,project_link TEXT,project_heading TEXT,project_disc TEXT,project_type TEXT,project_date TEXT)
+// CREATE TABLE basic_info(info_id INT PRIMARY KEY AUTO_INCREMENT,address TEXT,email TEXT,mobile TEXT)
--- route/user.js
+++ route/user.js
@@ -12,13 +12,16 @@ route.get("/about",async function(req,res){
     var social=await exe(`SELECT * FROM social`)
     var about_detail=await exe(`SELECT * FROM about_detail`)
     var service=await exe(`SELECT * FROM service`)
-    res.render("user/about.ejs",{"home_data":home_data,"social":social,"about_detail":about_detail,"service":service})
+    var programming=await exe(`SELECT * FROM programming`)
+    var interest=await exe(`SELECT * FROM interest`)
+    res.render("user/about.ejs",{"home_data":home_data,"social":social,"about_detail":about_detail,"service":service,"programming":programming,"interest":interest})
 })
 route.get("/all",async function(req,res){
     var home_data=await exe(`SELECT * FROM home`)
     var social=await exe(`SELECT * FROM social`)
+    var project=await exe(`SELECT * FROM project`)
 
-    res.render("user/all.ejs",{"home_data":home_data,"social":social})
+    res.render("user/all.ejs",{"home_data":home_data,"social":social,"project":project})
 })
 route.get("/frontend",async function(req,res){
     var home_data=await exe(`SELECT * FROM home`)
@@ -34,7 +37,26 @@ route.get("/fullstack",async function(req,res){
 route.get("/contact",async function(req,res){
     var home_data=await exe(`SELECT * FROM home`)
     var social=await exe(`SELECT * FROM social`)
+    var basic_info=await exe(`SELECT * FROM basic_info`)
+    res.render("user/contact.ejs",{"home_data":home_data,"social":social,"basic_info":basic_info})
+})
+route.post("/save_contact_form",async function(req,res){
+    var d=req.body;
+    var date=new Date().getDate()
+    var month=new Date().getMonth()+1;
+    var year=new Date().getFullYear()
+    var today=date+"/"+month+"/"+year
 
-    res.render("user/contact.ejs",{"home_data":home_data,"social":social})
+    var hour=new Date().getHours()%12
+    var minutes=new Date().getMinutes()+""
+    var second=new Date().getSeconds()
+    var time=(hour<10?'0'+hour:hour)+":"+(minutes<10?'0'+minutes:minutes)+":"+(second<10?'0'+second:second)
+
+    // console.log(time)
+    await exe(`INSERT INTO contact_form(Name,Email,subject,Message,date,time) VALUES('${d.Name}','${d.Email}','${d.subject}','${d.Message}','${today}','${time}')`)
+    res.redirect('/contact')
+    // res.send(req.body)
 })
 module.exports=route;
+
+// CREATE TABLE contact_form(c_id INT PRIMARY KEY AUTO_INCREMENT,Name VARCHAR(200),Email VARCHAR(200),subject TEXT,Message TEXT)
